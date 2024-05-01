842fc78a8a5a1d12de90ee820411a2e8117d66eb
Christian Lopierre <54386076+lopierrep@users.noreply.github.com>
2024-05-01 13:29:04 -0500

    Improve the score detection

--- Source/Skateboarding/Obstacles/Obstacle.cpp
+++ Source/Skateboarding/Obstacles/Obstacle.cpp
@@ -15,7 +15,7 @@ AObstacle::AObstacle()
 	BoxCollider = CreateDefaultSubobject<UBoxComponent>("BoxSphere");
 	BoxCollider->SetupAttachment(ObstacleMesh);
 	BoxCollider->SetBoxExtent(FVector(50.0f, 50.0f, 225.f), true);
-	BoxCollider->SetRelativeLocation(FVector(0.0f, 0.0f, 225.f));
+	BoxCollider->SetRelativeLocation(FVector(0.0f, 0.0f, 425.f));
 	BoxCollider->SetCollisionResponseToAllChannels(ECR_Ignore);
 	BoxCollider->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
 	\
