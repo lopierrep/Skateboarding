ff4ad83793e8bdbc507b46def91ddb5a42c10e44
Christian Lopierre <54386076+lopierrep@users.noreply.github.com>
2024-05-01 16:31:11 -0500

    Add SkateCharacter cpp

---
+++ Source/Skateboarding/Player/SkateCharacter.cpp
@@ -0,0 +1,98 @@
+#include "SkateCharacter.h"
+
+
+#include "Engine/LocalPlayer.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/Controller.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+#include "Skateboarding/Components/SkateMovementComponent.h"
+
+DEFINE_LOG_CATEGORY(LogTemplateCharacter);
+
+
+
+ASkateCharacter::ASkateCharacter()
+{
+
+	GetCapsuleComponent()->InitCapsuleSize(34.f, 90.0f);
+		
+	// Don't rotate when the controller rotates. Let that just affect the camera.
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	// Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = false;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
+
+	// instead of recompiling to adjust them
+	GetCharacterMovement()->JumpZVelocity = 1000.f;
+	GetCharacterMovement()->AirControl = 0.35f;
+	GetCharacterMovement()->MaxWalkSpeed = 800.f;
+	GetCharacterMovement()->MinAnalogWalkSpeed = 0.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
+
+	// Create a camera boom (pulls in towards the player if there is a collision)
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
+	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+
+	// Create a follow camera
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+}
+
+void ASkateCharacter::BeginPlay()
+{
+	// Call the base class  
+	Super::BeginPlay();
+	
+	SkateMovementComponent = FindComponentByClass<USkateMovementComponent>();
+	
+	//Add Input Mapping Context
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Input
+
+void ASkateCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	if (!SkateMovementComponent)
+	{
+		SkateMovementComponent = FindComponentByClass<USkateMovementComponent>();
+	}
+	SkateMovementComponent->SetupInputs(PlayerInputComponent);
+}
+
+void ASkateCharacter::Move(const FInputActionValue& Value)
+{
+
+}
+
+void ASkateCharacter::Look(const FInputActionValue& Value)
+{
+	// input is a Vector2D
+	const FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// add yaw and pitch input to controller
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
