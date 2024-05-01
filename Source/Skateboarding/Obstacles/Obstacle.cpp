23ee43e7c80e8f9e82f07eec3df09dc3c12d5d41
Christian Lopierre <54386076+lopierrep@users.noreply.github.com>
2024-05-01 13:50:11 -0500

    Clean code

--- Source/Skateboarding/Obstacles/Obstacle.cpp
+++ Source/Skateboarding/Obstacles/Obstacle.cpp
@@ -17,8 +17,7 @@ AObstacle::AObstacle()
 	BoxCollider->SetBoxExtent(FVector(50.0f, 50.0f, 225.f), true);
 	BoxCollider->SetRelativeLocation(FVector(0.0f, 0.0f, 425.f));
 	BoxCollider->SetCollisionResponseToAllChannels(ECR_Ignore);
-	BoxCollider->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
-	\
+	BoxCollider->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);	
 
 	ObstacleLogicComponent = CreateDefaultSubobject<UObstacleLogicComponent>("ObstacleLogicComponent");
 }
